426ab06740fe890e4519344c2a74cde4a4fb0887
Tuan Kiet <144747702+KietHTuan@users.noreply.github.com>
2025-08-30 22:36:35 -0700

    Hyperparameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,13 +43,48 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(), 
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(), 
-                "XGBClassifier": XGBRegressor(), 
-                "CatBoosting Classifier": CatBoostRegressor(verbose= False),
-                "AdaBoost Classifier": AdaBoostRegressor(), 
+                "KNN": KNeighborsRegressor(), 
+                "XGBoost": XGBRegressor(), 
+                "CatBoost": CatBoostRegressor(verbose= False),
+                "AdaBoost": AdaBoostRegressor(), 
             }
 
-            model_report: dict= evaluate_models(X_train= X_train, y_train= y_train, X_test= X_test,y_test = y_test,   models= models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "KNN": {
+                    "n_neighbors": [3,5,7,9],
+                    "weights": ["uniform", "distance"],
+                    "p": [1,2]
+                },
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+
+
+            model_report: dict= evaluate_models(X_train= X_train, y_train= y_train, X_test= X_test,y_test = y_test,   models= models, param= params)
 
             #best model score 
             best_model_score= max(sorted(model_report.values()))
